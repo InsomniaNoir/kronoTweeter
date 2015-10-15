20aa2d06eba03bf4039b8fd0e56196958a681614
Jeremiah Shafer <jrshafer@mac.com>
2015-10-14 23:39:49 -0500

    corrected issue with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/create'
-
   get 'pages/index'
   get 'auth/:provider/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   root to: "pages#index"
 end
